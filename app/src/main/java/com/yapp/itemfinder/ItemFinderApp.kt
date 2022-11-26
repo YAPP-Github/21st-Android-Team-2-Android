c58d6210dbdd1e27f4e6afe826b9c87ce84db246
Lee kijung <dlrlwjd1127@naver.com>
2022-11-26 13:12:21 +0900

    [A2Y-35] Add Hilt Dependency

--- app/src/main/java/com/yapp/itemfinder/ItemFinderApp.kt
+++ app/src/main/java/com/yapp/itemfinder/ItemFinderApp.kt
@@ -1,7 +1,15 @@
 package com.yapp.itemfinder
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class ItemFinderApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
 
 }
