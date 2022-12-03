bb3c4e72d9ab86ae5ff1cb510ebbd09d4397aa23
Lee kijung <dlrlwjd1127@naver.com>
2022-12-03 23:57:37 +0900

    [A2Y-42] Implement Data

---
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/model/CellType.kt
@@ -0,0 +1,7 @@
+package com.yapp.itemfinder.feature.common.datalist.model
+
+enum class CellType {
+
+    EMPTY_CELL,
+
+}
---
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/model/DataHandlerAlias.kt
@@ -0,0 +1,3 @@
+package com.yapp.itemfinder.feature.common.datalist.model
+
+typealias DataHandler = (Data) -> Unit
