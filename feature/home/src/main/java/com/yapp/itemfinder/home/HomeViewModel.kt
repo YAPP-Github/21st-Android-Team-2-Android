eb2bfe6cb806266130746f4344c6814da0ff7c0c
Lee kijung <dlrlwjd1127@naver.com>
2022-12-03 21:05:03 +0900

    [A2Y-37] Address from StateFlow to SharedFlow for chaning tabs

--- feature/home/src/main/java/com/yapp/itemfinder/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/yapp/itemfinder/home/HomeViewModel.kt
@@ -3,17 +3,17 @@ package com.yapp.itemfinder.home
 import androidx.annotation.IdRes
 import androidx.lifecycle.viewModelScope
 import com.yapp.itemfinder.feature.common.BaseViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.launch
 
 class HomeViewModel: BaseViewModel() {
 
-    private val _navigationItemStateFlow = MutableStateFlow<MainNavigation?>(null)
-    val navigationItemStateFlow: StateFlow<MainNavigation?> = _navigationItemStateFlow
+    private val _navigationItemStateFlow = MutableSharedFlow<MainNavigation?>()
+    val navigationItemStateFlow: SharedFlow<MainNavigation?> = _navigationItemStateFlow
 
     fun changeNavigation(mainNavigation: MainNavigation) = viewModelScope.launch {
-        _navigationItemStateFlow.value = mainNavigation
+        _navigationItemStateFlow.emit(mainNavigation)
     }
 
 }
