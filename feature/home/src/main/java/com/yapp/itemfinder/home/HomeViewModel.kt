5fa6466f366e464ef4a96e6b6c350776bdec8e23
Lee kijung <dlrlwjd1127@naver.com>
2022-12-03 18:59:41 +0900

    [A2Y-37] Implement HomeTabFragment from hometabs

--- feature/home/src/main/java/com/yapp/itemfinder/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/yapp/itemfinder/home/HomeViewModel.kt
@@ -1,8 +1,21 @@
 package com.yapp.itemfinder.home
 
+import androidx.annotation.IdRes
+import androidx.lifecycle.viewModelScope
 import com.yapp.itemfinder.feature.common.BaseViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class HomeViewModel: BaseViewModel() {
 
+    private val _navigationItemStateFlow = MutableStateFlow<MainNavigation?>(null)
+    val navigationItemStateFlow: StateFlow<MainNavigation?> = _navigationItemStateFlow
+
+    fun changeNavigation(mainNavigation: MainNavigation) = viewModelScope.launch {
+        _navigationItemStateFlow.value = mainNavigation
+    }
 
 }
+
+class MainNavigation(@IdRes val navigationMenuId: Int)
