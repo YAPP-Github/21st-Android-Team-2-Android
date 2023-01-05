85eecc017c3f4305e375e5a45334b3d3330ba707
EmilyCh0 <emily9803@ewhain.net>
2023-01-05 17:06:04 +0900

    [A2Y-152] popupmenu 스타일 및 아이콘 적용

--- feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/adapter/LockerViewHolder.kt
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/adapter/LockerViewHolder.kt
@@ -1,6 +1,9 @@
 package com.yapp.itemfinder.feature.common.datalist.adapter
 
+import android.os.Build
+import android.view.Gravity
 import android.widget.PopupMenu
+import androidx.annotation.RequiresApi
 import com.yapp.itemfinder.domain.model.Locker
 import com.yapp.itemfinder.feature.common.R
 import com.yapp.itemfinder.feature.common.databinding.LockerItemBinding
@@ -16,11 +19,12 @@ class LockerViewHolder(
         binding.lockerItemTextView.text = data.name
     }
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun bindViews(data: Locker) {
         binding.spinnerImageButton.setOnClickListener {
-            val con = itemView.context
-            val popupMenu = PopupMenu(itemView.context, binding.spinnerImageButton)
+            val popupMenu = PopupMenu(itemView.context, binding.spinnerImageButton, Gravity.END, 0, R.style.PopupMenu)
             popupMenu.inflate(R.menu.pop_up_menu)
+            popupMenu.setForceShowIcon(true)
             popupMenu.setOnMenuItemClickListener { menuItem ->
                 when(menuItem.itemId){
                     R.id.edit_menu -> {
