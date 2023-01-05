5d7b24eebd0ca47f7888e2db0d0b8a668fa082d1
EmilyCh0 <emily9803@ewhain.net>
2023-01-05 18:02:56 +0900

    [A2Y-152] conflict 해결

--- data/src/main/java/com/yapp/itemfinder/data/repositories/SpaceMockRepositoryImpl.kt
+++ data/src/main/java/com/yapp/itemfinder/data/repositories/SpaceMockRepositoryImpl.kt
@@ -13,40 +13,40 @@ class SpaceMockRepositoryImpl @Inject constructor() : SpaceRepository {
             SpaceItem(
                 name = "1 번째 공간",
                 lockerList = listOf(
-                    Locker(id = 1L, name = "화장대"),
+                    Locker(id = 1L, name = "화장대", com.yapp.itemfinder.domain.R.drawable.box),
                 )
             ),
             SpaceItem(
                 name = "2 번째 공간",
                 lockerList = listOf(
-                    Locker(id = 2L, name = "화장대"),
-                    Locker(id = 3L, name = "냉장고"),
+                    Locker(id = 2L, name = "화장대", com.yapp.itemfinder.domain.R.drawable.box),
+                    Locker(id = 3L, name = "냉장고", com.yapp.itemfinder.domain.R.drawable.box),
                 )
             ),
             SpaceItem(
                 name = "3 번째 공간",
                 lockerList = listOf(
-                    Locker(id = 4L, name = "화장대"),
-                    Locker(id = 5L, name = "냉장고"),
-                    Locker(id = 6L, name = "옷장"),
+                    Locker(id = 4L, name = "화장대", com.yapp.itemfinder.domain.R.drawable.box),
+                    Locker(id = 5L, name = "냉장고", com.yapp.itemfinder.domain.R.drawable.box),
+                    Locker(id = 6L, name = "옷장", com.yapp.itemfinder.domain.R.drawable.box),
                 )
             ),
             SpaceItem(
                 name = "4 번째 공간",
                 lockerList = listOf(
-                    Locker(id = 7L, name = "화장대"),
-                    Locker(id = 8L, name = "냉장고"),
-                    Locker(id = 9L, name = "옷장"),
-                    Locker(id = 10L, name = "옷장"),
+                    Locker(id = 7L, name = "화장대", com.yapp.itemfinder.domain.R.drawable.box),
+                    Locker(id = 8L, name = "냉장고", com.yapp.itemfinder.domain.R.drawable.box),
+                    Locker(id = 9L, name = "옷장", com.yapp.itemfinder.domain.R.drawable.box),
+                    Locker(id = 10L, name = "옷장", com.yapp.itemfinder.domain.R.drawable.box),
                 )
             ),
             SpaceItem(
-                name = "$5 번째 공간",
+                name = "5 번째 공간",
                 lockerList = listOf(
-                    Locker(id = 11L, Math.random().toString()),
-                    Locker(id = 12L, Math.random().toString()),
-                    Locker(id = 13L, Math.random().toString()),
-                    Locker(id = 14L, Math.random().toString())
+                    Locker(id = 1L,Math.random().toString(), com.yapp.itemfinder.domain.R.drawable.box),
+                    Locker(id = 2L,Math.random().toString(), com.yapp.itemfinder.domain.R.drawable.box),
+                    Locker(id = 3L,Math.random().toString(), com.yapp.itemfinder.domain.R.drawable.box),
+                    Locker(id = 4L,Math.random().toString(), com.yapp.itemfinder.domain.R.drawable.box)
                 )
             )
         )
