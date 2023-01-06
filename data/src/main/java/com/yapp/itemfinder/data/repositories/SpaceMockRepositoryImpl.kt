5894a91d1ea43b18368f2ba812592aa241f68782
BraveSejin <ihavebravebiz@gmail.com>
2023-01-07 01:21:52 +0900

    [A2Y-154] HomeTabFragment 디자인 적용

--- data/src/main/java/com/yapp/itemfinder/data/repositories/SpaceMockRepositoryImpl.kt
+++ data/src/main/java/com/yapp/itemfinder/data/repositories/SpaceMockRepositoryImpl.kt
@@ -11,42 +11,42 @@ class SpaceMockRepositoryImpl @Inject constructor() : SpaceRepository {
     override fun getAllSpace(): List<SpaceItem> {
         return listOf(
             SpaceItem(
-                name = "$0 번째 공간",
+                name = "0 번째 공간",
                 lockerList = listOf(
                 )
             ),
             SpaceItem(
-                name = "$1 번째 공간",
-                lockerList = listOf(
-                    Locker(name = "화장대", "https://picsum.photos/200"),
-                )
-            ),
-            SpaceItem(
-                name = "$2 번째 공간",
+                name = "1 번째 공간",
                 lockerList = listOf(
                     Locker(name = "화장대", "https://picsum.photos/200"),
-                    Locker(name = "냉장고", "https://picsum.photos/200"),
                 )
             ),
             SpaceItem(
-                name = "$3 번째 공간",
+                name = "2 번째 공간",
                 lockerList = listOf(
                     Locker(name = "화장대", "https://picsum.photos/200"),
                     Locker(name = "냉장고", "https://picsum.photos/200"),
-                    Locker(name = "옷장", "https://picsum.photos/200"),
                 )
             ),
             SpaceItem(
-                name = "$4 번째 공간",
+                name = "3 번째 공간",
                 lockerList = listOf(
                     Locker(name = "화장대", "https://picsum.photos/200"),
                     Locker(name = "냉장고", "https://picsum.photos/200"),
                     Locker(name = "옷장", "https://picsum.photos/200"),
+                )
+            ),
+            SpaceItem(
+                name = "4 번째 공간",
+                lockerList = listOf(
+                    Locker(name = "화장대", "https://picsum.photos/200"),
+                    Locker(name = "냉장고", "https://picsum.photos/200"),
+                    Locker(name = "옷장", "https://picsum.photos/200"),
                     Locker(name = "옷장", "https://picsum.photos/200"),
                 )
             ),
             SpaceItem(
-                name = "$5 번째 공간",
+                name = "5 번째 공간",
                 lockerList = listOf(
                     Locker(name = "화장대", "https://picsum.photos/200"),
                     Locker(name = "냉장고", "https://picsum.photos/200"),
--- feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/adapter/SpaceViewHolder.kt
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/adapter/SpaceViewHolder.kt
@@ -1,9 +1,12 @@
 package com.yapp.itemfinder.feature.common.datalist.adapter
 
+import android.graphics.drawable.Drawable
+import android.view.View
 import com.bumptech.glide.Glide
 import com.yapp.itemfinder.domain.model.SpaceItem
 import com.yapp.itemfinder.feature.common.R
 import com.yapp.itemfinder.feature.common.databinding.SpaceItemBinding
+import com.yapp.itemfinder.feature.common.extension.visible
 
 class SpaceViewHolder(
     val binding: SpaceItemBinding
@@ -11,18 +14,34 @@ class SpaceViewHolder(
 
     override fun bindData(data: SpaceItem) {
         super.bindData(data)
+        val spaceName = data.name
+        val lockersInSpace = data.lockerList
         with(binding) {
-            spaceItemName.text = data.name
+            spaceItemName.text = spaceName
 
-            data.lockerList.run {
-                getOrNull(0)?.let { Glide.with(itemView).load(it.url).into(spaceFirstImageView) }
-                getOrNull(1)?.let { Glide.with(itemView).load(it.url).into(spaceSecondViewImage) }
-                getOrNull(2)?.let { Glide.with(itemView).load(it.url).into(spaceThirdImageView) }
-                if (data.lockerList.size == 4){
-                    getOrNull(3)?.let { Glide.with(itemView).load(R.drawable.arrow_forward_16).into(spaceFourthImageView) }
-                }else{
-                     Glide.with(itemView).load(R.drawable.arrow_forward_16).into(spaceFourthImageView)
+            val imageViews = listOf(spaceFirstImageView, spaceSecondImageView, spaceThirdImageView, spaceFourthImageView)
+            val frames = listOf(spaceFirstFrame, spaceSecondFrame, spaceThirdFrame, spaceFourthFrame)
+
+            for (i in lockersInSpace.indices){
+
+                val frame = frames[i]
+                val imageView = imageViews[i]
+
+                when(i){
+                    0,1,2 -> {
+                        Glide.with(itemView).load(R.drawable.box).into(imageView)
+                    }
+                    3 ->{
+                        if (lockersInSpace.size > 4){
+                            spaceFourthTextView.text = "+${lockersInSpace.size-3}"
+                            frame.setCardBackgroundColor(frame.context.getColor(R.color.brown_03))
+                        }else{
+                            Glide.with(itemView).load(R.drawable.box).into(imageView)
+                        }
+                    }
                 }
+                frame.visible()
+                if (i == 3) break
             }
         }
     }
