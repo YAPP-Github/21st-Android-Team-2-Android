f5696ca1aab578acd0f63638c06932f8721e0d6a
EmilyCh0 <emily9803@ewhain.net>
2023-01-07 13:48:59 +0900

    [A2Y-152] 테스트 코드 삭제

--- domain/src/main/java/com/yapp/itemfinder/domain/model/AddLocker.kt
+++ domain/src/main/java/com/yapp/itemfinder/domain/model/AddLocker.kt
@@ -2,7 +2,7 @@ package com.yapp.itemfinder.domain.model
 
 data class AddLocker(
     override var type: CellType = CellType.ADD_LOCKER_CELL
-): Data(){
+) : Data() {
     var addLockerHandler: DataHandler = {}
     fun addLocker() = addLockerHandler.invoke(this)
 }
