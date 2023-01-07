ecd2243c71ea3406b0456dda6df28ed43bc82b02
Lee kijung <dlrlwjd1127@naver.com>
2023-01-07 17:21:14 +0900

    A2Y-134 Implement DispatcherProvider

---
+++ app/src/main/java/com/yapp/itemfinder/provider/DefaultDispatcherProvider.kt
@@ -0,0 +1,19 @@
+package com.yapp.itemfinder.provider
+
+import com.yapp.itemfinder.domain.coroutines.DispatcherProvider
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DefaultDispatcherProvider @Inject constructor(): DispatcherProvider {
+    override val default: CoroutineDispatcher
+        get() = Dispatchers.Default
+    override val io: CoroutineDispatcher
+        get() = Dispatchers.IO
+    override val main: CoroutineDispatcher
+        get() = Dispatchers.Main
+    override val mainImmediate: CoroutineDispatcher
+        get() = Dispatchers.Main.immediate
+}
