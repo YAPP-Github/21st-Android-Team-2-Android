1bbafbaa7cdad5ff4877ace0d7680ee61d43e6eb
BraveSejin <ihavebravebiz@gmail.com>
2023-01-07 17:59:04 +0900

    [A2Y-179] item-decoration-dpToPx

---
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/extension/Extension.kt
@@ -0,0 +1,8 @@
+package com.yapp.itemfinder.feature.common.extension
+
+import android.content.Context
+import android.util.DisplayMetrics
+
+fun Int.dpToPx(context: Context): Int{
+    return this * (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT).toInt()
+}
