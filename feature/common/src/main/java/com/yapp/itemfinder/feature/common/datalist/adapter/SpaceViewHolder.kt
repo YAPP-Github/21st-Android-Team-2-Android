b9f449d2deeafcb795780eb692ddaef82fcd2bc3
BraveSejin <ihavebravebiz@gmail.com>
2023-01-07 14:40:31 +0900

    [A2Y-154] SpaceViewHolder refactoring

--- feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/adapter/SpaceViewHolder.kt
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/adapter/SpaceViewHolder.kt
@@ -3,45 +3,56 @@ package com.yapp.itemfinder.feature.common.datalist.adapter
 import android.graphics.drawable.Drawable
 import android.view.View
 import com.bumptech.glide.Glide
+import com.yapp.itemfinder.domain.model.Locker
 import com.yapp.itemfinder.domain.model.SpaceItem
 import com.yapp.itemfinder.feature.common.R
 import com.yapp.itemfinder.feature.common.databinding.SpaceItemBinding
+import com.yapp.itemfinder.feature.common.extension.invisible
 import com.yapp.itemfinder.feature.common.extension.visible
 
 class SpaceViewHolder(
     val binding: SpaceItemBinding
 ) : DataViewHolder<SpaceItem>(binding) {
+    lateinit var spaceName: String
+    lateinit var lockersInSpace: List<Locker>
+    private val imageViews = listOf(
+        binding.spaceFirstImageView,
+        binding.spaceSecondImageView,
+        binding.spaceThirdImageView,
+        binding.spaceFourthImageView
+    )
+    private val frames = listOf(
+        binding.spaceFirstFrame,
+        binding.spaceSecondFrame,
+        binding.spaceThirdFrame,
+        binding.spaceFourthFrame
+    )
 
     override fun bindData(data: SpaceItem) {
         super.bindData(data)
-        val spaceName = data.name
-        val lockersInSpace = data.lockerList
+        spaceName = data.name
+        lockersInSpace = data.lockerList
         with(binding) {
             spaceItemName.text = spaceName
 
-            val imageViews = listOf(spaceFirstImageView, spaceSecondImageView, spaceThirdImageView, spaceFourthImageView)
-            val frames = listOf(spaceFirstFrame, spaceSecondFrame, spaceThirdFrame, spaceFourthFrame)
-
-            for (i in lockersInSpace.indices){
-
-                val frame = frames[i]
-                val imageView = imageViews[i]
-
-                when(i){
-                    0,1,2 -> {
+            lockersInSpace.indices.forEach { idx ->
+                val frame = frames[idx]
+                val imageView = imageViews[idx]
+                frame.visible()
+                when (idx) {
+                    0, 1, 2 -> {
                         Glide.with(itemView).load(R.drawable.box).into(imageView)
                     }
-                    3 ->{
-                        if (lockersInSpace.size > 4){
-                            spaceFourthTextView.text = "+${lockersInSpace.size-3}"
+                    3 -> {
+                        if (lockersInSpace.size > 4) {
+                            spaceFourthTextView.text = "+${lockersInSpace.size - 3}"
                             frame.setCardBackgroundColor(frame.context.getColor(R.color.brown_03))
-                        }else{
+                        } else {
                             Glide.with(itemView).load(R.drawable.box).into(imageView)
                         }
+                        return
                     }
                 }
-                frame.visible()
-                if (i == 3) break
             }
         }
     }
@@ -51,6 +62,6 @@ class SpaceViewHolder(
     }
 
     override fun reset() {
-
+        frames.forEach { it.invisible() }
     }
 }
