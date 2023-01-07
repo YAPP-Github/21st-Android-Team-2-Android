037febb017f44d503828b6ee04e9e3089fd2451b
EmilyCh0 <emily9803@ewhain.net>
2023-01-07 17:49:49 +0900

    [A2Y-177] space 모듈로 변경

--- feature/locker/src/main/java/com/yapp/itemfinder/locker/LockerListSideEffect.kt
+++
@@ -1,8 +0,0 @@
-package com.yapp.itemfinder.locker
-
-import com.yapp.itemfinder.domain.model.Locker
-import com.yapp.itemfinder.feature.common.SideEffect
-
-sealed class LockerListSideEffect: SideEffect {
-    data class MoveToLockerDetail(val locker: Locker): LockerListSideEffect()
-}
---
+++ feature/space/src/main/java/com/yapp/itemfinder/space/binder/LockerAddLockerItemBinder.kt
@@ -0,0 +1,22 @@
+package com.yapp.itemfinder.space.binder
+
+import com.yapp.itemfinder.domain.model.AddLocker
+import com.yapp.itemfinder.feature.common.BaseViewModel
+import com.yapp.itemfinder.feature.common.datalist.binder.AddLockerItemBinder
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class LockerAddLockerItemBinder @Inject constructor() : AddLockerItemBinder {
+    override fun bindData(data: AddLocker, viewModel: BaseViewModel) {
+        when (viewModel){
+            is com.yapp.itemfinder.space.LockerListViewModel -> setLockerListViewModelHandler(data, viewModel)
+        }
+    }
+
+    private fun setLockerListViewModelHandler(item: AddLocker, viewModel: com.yapp.itemfinder.space.LockerListViewModel) {
+        item.addLockerHandler = { data ->
+            viewModel.addItem()
+        }
+    }
+}
