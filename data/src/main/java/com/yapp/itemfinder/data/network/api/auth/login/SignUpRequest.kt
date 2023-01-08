cef3e50f654241f2976b3e20a8243830ec5f38f0
Lee kijung <dlrlwjd1127@naver.com>
2023-01-09 02:15:13 +0900

    A2Y-182 Implement Login & SignUp API in AuthRepository

--- data/src/main/java/com/yapp/itemfinder/data/network/api/auth/login/SignUpRequest.kt
+++ data/src/main/java/com/yapp/itemfinder/data/network/api/auth/login/SignUpRequest.kt
@@ -1,10 +1,28 @@
 package com.yapp.itemfinder.data.network.api.auth.login
 
+import com.google.gson.annotations.SerializedName
+import com.yapp.itemfinder.domain.entity.signup.SignUpEntity
+
 data class SignUpRequest(
-    val socialId: String,
-    val socialType: String,
+    @SerializedName("socialId") val socialId: String,
+    @SerializedName("socialType") val socialType: String,
     val nickname: String?,
     val email: String?,
     val gender: String?,
     val birthYear: Int?
-)
+) {
+
+    companion object {
+
+        fun fromEntity(signUpEntity: SignUpEntity) = SignUpRequest(
+            socialId = signUpEntity.socialId,
+            socialType = signUpEntity.socialType.name,
+            nickname = signUpEntity.nickname,
+            email = signUpEntity.email,
+            gender = signUpEntity.gender?.name,
+            birthYear = signUpEntity.birthYear
+        )
+
+    }
+
+}
--- data/src/main/java/com/yapp/itemfinder/data/network/api/auth/login/SignUpResponse.kt
+++ data/src/main/java/com/yapp/itemfinder/data/network/api/auth/login/SignUpResponse.kt
@@ -1,6 +1,12 @@
 package com.yapp.itemfinder.data.network.api.auth.login
 
+import com.yapp.itemfinder.domain.entity.auth.AuthTokenEntity
+
 data class SignUpResponse(
     val accessToken: String,
     val refreshToken: String
-)
+) {
+
+    fun toAuthTokenEntity() = AuthTokenEntity(accessToken, refreshToken)
+
+}
