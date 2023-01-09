7ee648018257b25a58414015464c624e8cd72ccc
BraveSejin <ihavebravebiz@gmail.com>
2023-01-09 21:41:21 +0900

    [A2Y-159]range 관련 확장함수 추가

--- feature/common/src/main/java/com/yapp/itemfinder/feature/common/extension/Extension.kt
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/extension/Extension.kt
@@ -6,3 +6,10 @@ import android.util.DisplayMetrics
 fun Int.dpToPx(context: Context): Int{
     return this * (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT).toInt()
 }
+
+
+fun IntRange.size() = this.last - this.first + 1
+
+fun Int.isOdd() = this % 2 == 1
+
+fun Int.isEven() = this % 2 == 0
