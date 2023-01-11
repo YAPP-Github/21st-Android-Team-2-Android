50d9fb095e72f33e8dd373aaa072c69faaeef111
BraveSejin <ihavebravebiz@gmail.com>
2023-01-12 00:11:29 +0900

    fix fragmentNavigator to have bundle

--- feature/common/src/main/java/com/yapp/itemfinder/feature/common/FragmentNavigator.kt
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/FragmentNavigator.kt
@@ -1,6 +1,8 @@
 package com.yapp.itemfinder.feature.common
 
+import android.os.Parcelable
+
 interface FragmentNavigator {
     fun showFragment(tag: String)
-    fun addFragmentBackStack(tag: String)
+    fun addFragmentBackStack(tag: String, bundlePair:Pair<String,Parcelable>? = null)
 }
