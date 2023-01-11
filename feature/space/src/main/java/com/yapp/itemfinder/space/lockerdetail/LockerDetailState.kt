c051c08abb09ab472be4e470500943bfa3b6714d
BraveSejin <ihavebravebiz@gmail.com>
2023-01-11 22:23:49 +0900

    [A2Y-196] setup files

--- feature/space/src/main/java/com/yapp/itemfinder/space/lockerdetail/LockerDetailState.kt
+++ feature/space/src/main/java/com/yapp/itemfinder/space/lockerdetail/LockerDetailState.kt
@@ -1,4 +1,17 @@
 package com.yapp.itemfinder.space.lockerdetail
+import com.yapp.itemfinder.domain.model.Data
+import com.yapp.itemfinder.feature.common.State
 
-class LockerDetailState {
+sealed class LockerDetailState: State {
+    object Uninitialized : LockerDetailState()
+
+    object Loading : LockerDetailState()
+
+    data class Success(
+        val dataList: List<Data>
+    ) : LockerDetailState()
+
+    data class Error(
+        val e: Throwable
+    ) : LockerDetailState()
 }
