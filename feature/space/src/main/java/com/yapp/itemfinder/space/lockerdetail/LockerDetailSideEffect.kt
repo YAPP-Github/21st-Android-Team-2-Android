caa7c0304b9e13505cf1171481fb835c40c9868e
BraveSejin <ihavebravebiz@gmail.com>
2023-01-11 22:23:49 +0900

    [A2Y-196] setup files

--- feature/space/src/main/java/com/yapp/itemfinder/space/lockerdetail/LockerDetailSideEffect.kt
+++ feature/space/src/main/java/com/yapp/itemfinder/space/lockerdetail/LockerDetailSideEffect.kt
@@ -1,4 +1,7 @@
 package com.yapp.itemfinder.space.lockerdetail
 
-class LockerDetailSideEffect {
+import com.yapp.itemfinder.domain.model.Locker
+import com.yapp.itemfinder.feature.common.SideEffect
+
+sealed class LockerDetailSideEffect: SideEffect {
 }
