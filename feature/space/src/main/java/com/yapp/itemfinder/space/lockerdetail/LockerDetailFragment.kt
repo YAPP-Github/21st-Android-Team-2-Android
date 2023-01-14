2fe8e0e1dc0c85df0f6ce5300272e43aea7d1227
BraveSejin <ihavebravebiz@gmail.com>
2023-01-14 22:35:46 +0900

    [A2Y-196] apply bottom sheet drag policy (mutating toggle image)

--- feature/space/src/main/java/com/yapp/itemfinder/space/lockerdetail/LockerDetailFragment.kt
+++ feature/space/src/main/java/com/yapp/itemfinder/space/lockerdetail/LockerDetailFragment.kt
@@ -1,12 +1,17 @@
 package com.yapp.itemfinder.space.lockerdetail
 
+import android.annotation.SuppressLint
+import android.view.View
 import androidx.lifecycle.*
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
 import com.yapp.itemfinder.domain.model.Data
 import com.yapp.itemfinder.feature.common.BaseStateFragment
 import com.yapp.itemfinder.feature.common.binding.viewBinding
 import com.yapp.itemfinder.feature.common.datalist.adapter.DataListAdapter
 import com.yapp.itemfinder.feature.common.datalist.binder.DataBindHelper
 import com.yapp.itemfinder.feature.common.extension.showShortToast
+import com.yapp.itemfinder.space.R
 import com.yapp.itemfinder.space.databinding.FragmentLockerDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
@@ -14,7 +19,8 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class LockerDetailFragment : BaseStateFragment<LockerDetailViewModel, FragmentLockerDetailBinding>() {
+class LockerDetailFragment :
+    BaseStateFragment<LockerDetailViewModel, FragmentLockerDetailBinding>() {
 
     override val vm by lazy {
         ViewModelProvider(
@@ -34,6 +40,45 @@ class LockerDetailFragment : BaseStateFragment<LockerDetailViewModel, FragmentLo
         if (dataListAdapter == null) {
             dataListAdapter = DataListAdapter()
         }
+        initBottomSheet()
+    }
+
+    private fun initBottomSheet() {
+        setBottomSheetBehavior()
+        blockBottomSheetTouchIntercept()
+    }
+
+    /**
+     * 리사이클러뷰 부모가 터치이벤트를 인터셉트하는 것을 방지합니다. 리사이클러뷰 스크롤을 해야 하는데,
+     */
+
+    private fun setBottomSheetBehavior() {
+        val behavior = BottomSheetBehavior.from(binding.bottomSheet.root)
+        behavior.addBottomSheetCallback(object : BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                with(binding.bottomSheet.bottomSheetToggleImageView) {
+                    if (newState == BottomSheetBehavior.STATE_EXPANDED)
+                        setImageResource(R.drawable.toggle_down)
+                    else if (newState == BottomSheetBehavior.STATE_COLLAPSED)
+                        setImageResource(R.drawable.toggle_up)
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                return
+            }
+        })
+        behavior.peekHeight = binding.lockerDetailImageView.height
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun blockBottomSheetTouchIntercept() {
+        with(binding.bottomSheet) {
+            rcvDouble.setOnTouchListener { _, _ ->
+                rcvDouble.parent.requestDisallowInterceptTouchEvent(true)
+                return@setOnTouchListener false
+            }
+        }
     }
 
     override fun observeData(): Job {
@@ -51,12 +96,7 @@ class LockerDetailFragment : BaseStateFragment<LockerDetailViewModel, FragmentLo
                 }
                 launch {
                     vm.sideEffectFlow.collect { sideEffect ->
-//                        when (sideEffect) {
-//                            is LockerListSideEffect.MoveToLockerDetail -> {
-//                                // 이동
-//                            }
-//                            else -> {}
-//                        }
+//
                     }
                 }
             }
@@ -64,9 +104,8 @@ class LockerDetailFragment : BaseStateFragment<LockerDetailViewModel, FragmentLo
         return job
     }
 
-    private fun handleUninitialized(lockerDetailState: LockerDetailState.Uninitialized){
+    private fun handleUninitialized(lockerDetailState: LockerDetailState.Uninitialized) {
         requireActivity().showShortToast(lockerDetailState.locker.toString())
-
     }
 
 //    private fun handleLoading(lockerListState: LockerListState) {
