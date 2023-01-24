06701099be2edcabce2c6c1818a763257c144153
BraveSejin <ihavebravebiz@gmail.com>
2023-01-24 20:58:53 +0900

    [A2Y-194] 사진 업로드시 UI radius 적용, 사진 아이콘 적용

--- feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/adapter/AddLockerPhotoViewHolder.kt
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/adapter/AddLockerPhotoViewHolder.kt
@@ -1,8 +1,12 @@
 package com.yapp.itemfinder.feature.common.datalist.adapter
 
 import android.net.Uri
+import android.view.RoundedCorner
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.yapp.itemfinder.domain.model.AddLockerPhoto
+import com.yapp.itemfinder.feature.common.R
 import com.yapp.itemfinder.feature.common.databinding.AddLockerPhotoBinding
 import com.yapp.itemfinder.feature.common.extension.gone
 import com.yapp.itemfinder.feature.common.extension.visible
@@ -10,6 +14,7 @@ import com.yapp.itemfinder.feature.common.extension.visible
 class AddLockerPhotoViewHolder(
     val binding: AddLockerPhotoBinding
 ) : DataViewHolder<AddLockerPhoto>(binding) {
+    private val context = binding.root.context
     override fun reset() {
         return
     }
@@ -18,12 +23,23 @@ class AddLockerPhotoViewHolder(
         super.bindData(data)
         with(binding) {
             data.uriString?.let {
+                photoIconImageView.visible()
+                photoImageView.visible()
+
                 addPhotoButton.gone()
                 addPhotoDescriptionTextView.gone()
-                photoImageView.visible()
                 Glide.with(photoImageView).load(Uri.parse(it))
-                    .centerCrop()
+                    .transform(
+                        CenterCrop(),
+                        RoundedCorners(context.resources.getDimension(R.dimen.add_locker_image_radius).toInt())
+                    )
                     .into(photoImageView)
+            } ?: kotlin.run {
+                photoIconImageView.gone()
+                photoImageView.gone()
+
+                addPhotoButton.visible()
+                addPhotoDescriptionTextView.visible()
             }
         }
     }
