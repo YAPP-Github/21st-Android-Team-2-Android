23a2ea187fffc0d8079d1287a617424149b35a04
BraveSejin <ihavebravebiz@gmail.com>
2023-01-29 20:18:00 +0900

    [A2Y-233] add cancel image handler

--- domain/src/main/java/com/yapp/itemfinder/domain/model/AddItemImages.kt
+++ domain/src/main/java/com/yapp/itemfinder/domain/model/AddItemImages.kt
@@ -1,10 +1,13 @@
 package com.yapp.itemfinder.domain.model
 
 data class AddItemImages(
-    var uriStringList: List<String>,
+    var uriStringList: MutableList<String>,
     override var type: CellType = CellType.ADD_ITEM_IMAGES_CELL,
     val maxCount: Int = 10
 ): Data(){
     var addCameraActionHandler: () -> Unit = {}
     fun addCameraAction() = addCameraActionHandler.invoke()
+
+    var cancelImageUploadHandler: () -> Unit = {}
+    fun cancelImageUpload() = cancelImageUploadHandler.invoke()
 }
