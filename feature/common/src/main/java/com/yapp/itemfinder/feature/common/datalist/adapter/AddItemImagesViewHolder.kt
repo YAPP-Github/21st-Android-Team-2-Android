6ca76762ae7d1b674e69f8dd00a1fc3243556d67
BraveSejin <ihavebravebiz@gmail.com>
2023-01-29 14:59:47 +0900

    [A2Y-194] item decoration margin 수정

--- feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/adapter/AddItemImagesViewHolder.kt
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/datalist/adapter/AddItemImagesViewHolder.kt
@@ -1,14 +1,19 @@
 package com.yapp.itemfinder.feature.common.datalist.adapter
 
 import com.yapp.itemfinder.domain.model.AddItemImages
+import com.yapp.itemfinder.feature.common.R
 import com.yapp.itemfinder.feature.common.adaper.AddItemImagesInnerAdapter
 import com.yapp.itemfinder.feature.common.databinding.AddItemImagesBinding
-import com.yapp.itemfinder.feature.common.utility.LastItemRightMarginZeroDecoration
+import com.yapp.itemfinder.feature.common.utility.AddImagesItemDecoration
 
 class AddItemImagesViewHolder(
     val binding: AddItemImagesBinding
 ) : DataViewHolder<AddItemImages>(binding) {
     lateinit var mAdapter: AddItemImagesInnerAdapter
+    private val context = binding.root.context
+    private val addImagesDecoration by lazy {
+        AddImagesItemDecoration(context.resources.getDimension(R.dimen.add_images_recyclerview_margin).toInt())
+    }
 
     override fun reset() {
         return
@@ -41,6 +46,6 @@ class AddItemImagesViewHolder(
     }
 
     override fun bindViews(data: AddItemImages) {
-        binding.innerRecyclerView.addItemDecoration(LastItemRightMarginZeroDecoration())
+        binding.innerRecyclerView.addItemDecoration(addImagesDecoration)
     }
 }
--- feature/common/src/main/java/com/yapp/itemfinder/feature/common/utility/LastItemMarginDecorator.kt
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/utility/LastItemMarginDecorator.kt
@@ -4,8 +4,9 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import com.yapp.itemfinder.feature.common.extension.dpToPx
 
-internal class LastItemRightMarginZeroDecoration() :
+class AddImagesItemDecoration(private val marginBetween: Int) :
     ItemDecoration() {
     override fun getItemOffsets(
         outRect: Rect,
@@ -14,8 +15,13 @@ internal class LastItemRightMarginZeroDecoration() :
         state: RecyclerView.State
     ) {
         val position = parent.getChildAdapterPosition(view)
-        if (position == state.itemCount - 1) {
+        if (position == 0){ // cancel버튼이 없습니다.
+            outRect.right = marginBetween
+        }
+        else if (position == state.itemCount - 1) { // 마지막 부분은 마진을 주지 않습니다. ㅁㄴ
             outRect.right = 0
+        }else{ // 마진의 절반은 cancel 버튼이 차지합니다.
+            outRect.right = marginBetween / 2
         }
     }
 }
