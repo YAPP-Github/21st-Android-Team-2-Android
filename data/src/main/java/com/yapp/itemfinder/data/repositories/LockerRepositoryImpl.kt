4a0829f868867827e0a5511da8f29d12dbab6884
Lee kijung <dlrlwjd1127@naver.com>
2023-02-05 14:50:18 +0900

    [A2Y-249] Resolve Conflicts

--- data/src/main/java/com/yapp/itemfinder/data/repositories/LockerRepositoryImpl.kt
+++ data/src/main/java/com/yapp/itemfinder/data/repositories/LockerRepositoryImpl.kt
@@ -1,9 +1,7 @@
 package com.yapp.itemfinder.data.repositories
 
-import com.yapp.itemfinder.data.network.api.lockerlist.LockerListApi
 import com.yapp.itemfinder.domain.model.AddLockerRequest
 import com.yapp.itemfinder.data.network.api.lockerlist.LockerApi
-import com.yapp.itemfinder.domain.coroutines.DispatcherProvider
 import com.yapp.itemfinder.domain.model.LockerEntity
 import com.yapp.itemfinder.domain.repository.LockerRepository
 import javax.inject.Inject
@@ -11,12 +9,11 @@ import javax.inject.Singleton
 
 @Singleton
 class LockerRepositoryImpl @Inject constructor(
-    private val lockerListApi: LockerListApi,
     private val lockerApi: LockerApi
 ) : LockerRepository {
 
     override suspend fun getLockers(spaceId: Long): List<LockerEntity> =
-        lockerListApi.getLockersBySpaceId(spaceId).map { it.refineToLocker() }
+        lockerApi.getLockersBySpaceId(spaceId).map { it.refineToLocker() }
 
     override suspend fun addLocker(locker: AddLockerRequest): LockerEntity =
         lockerApi.addNewLocker(locker).refineToLocker()
