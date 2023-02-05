7efa657d1e12d6012e431a8359512b3e0cdb4044
Lee kijung <dlrlwjd1127@naver.com>
2023-02-05 14:12:51 +0900

    [A2Y-249] Fix name save in AddLockerViewModel

--- domain/src/main/java/com/yapp/itemfinder/domain/model/AddLockerNameInput.kt
+++ domain/src/main/java/com/yapp/itemfinder/domain/model/AddLockerNameInput.kt
@@ -3,6 +3,10 @@ package com.yapp.itemfinder.domain.model
 data class AddLockerNameInput(
     override var type: CellType = CellType.ADD_LOCKER_NAME_INPUT_CELL
 ) : Data() {
-    var enterHandler: EnterStringHandler = {}
+    var enterHandler: StringHandler = {}
     fun enterName(name: String) = enterHandler.invoke(name)
+
+    var saveHandler: ActionHandler = { }
+    fun saveName() = saveHandler.invoke()
+
 }
