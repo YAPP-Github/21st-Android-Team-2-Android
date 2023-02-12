f313c032a2f2c7d8ba3f3d43a892acace689b980
BraveSejin <ihavebravebiz@gmail.com>
2023-02-12 22:46:00 +0900

    [A2Y-251] add item req/res  수정

--- data/src/main/java/com/yapp/itemfinder/data/network/api/item/AddItemResponse.kt
+++ data/src/main/java/com/yapp/itemfinder/data/network/api/item/AddItemResponse.kt
@@ -1,10 +1,12 @@
 package com.yapp.itemfinder.data.network.api.item
 
+import com.yapp.itemfinder.domain.model.Item
+import com.yapp.itemfinder.domain.model.Tag
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class AddItemResponse(
-//    val id: Long, //fixme: 추가예정
+    val id: Long,
     val name: String,
     val itemType: String,
     val quantity: Int,
@@ -15,4 +17,17 @@ data class AddItemResponse(
     val useByDate: LocalDateTime?,
     val pinX: Float?,
     val pinY: Float?
-)
+){
+    fun toItem(): Item {
+        return Item(
+            id = id,
+            name = name,
+            count = quantity,
+            imageUrls = imageUrls,
+            tags= tags.map { Tag(it) },
+            memo = description,
+            position = if (pinX != null && pinY != null) Item.Position(pinX, pinY) else null
+        )
+    }
+
+}
