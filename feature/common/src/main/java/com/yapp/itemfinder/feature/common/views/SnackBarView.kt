d66d54955aa5bdfd4a2b2148af5fc1dcc66da16c
BraveSejin <ihavebravebiz@gmail.com>
2023-02-18 01:39:21 +0900

    [A2Y-269] 데이터바인딩 비활성화 및 SnackbarView를 ViewBinding으로 변경

--- feature/common/src/main/java/com/yapp/itemfinder/feature/common/views/SnackBarView.kt
+++ feature/common/src/main/java/com/yapp/itemfinder/feature/common/views/SnackBarView.kt
@@ -1,40 +1,31 @@
 package com.yapp.itemfinder.feature.common.views
 
 import android.view.LayoutInflater
-import android.view.View
+import android.view.ViewGroup
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
 import com.google.android.material.snackbar.Snackbar
 import com.yapp.itemfinder.feature.common.R
 import com.yapp.itemfinder.feature.common.databinding.SnackbarBinding
 
 class SnackBarView(
-    val view: View,
-    private val message: String
+    viewGroup: ViewGroup,
+    message: String
 ) {
 
-    private val context = view.context
-    private val snackBar = Snackbar.make(view, "", Snackbar.LENGTH_SHORT)
+    private val context = viewGroup.context
+    private val snackBar = Snackbar.make(viewGroup, "", Snackbar.LENGTH_SHORT)
     private val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
 
     private val snackBarBinding: SnackbarBinding =
-        DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.snackbar, null, false)
+        SnackbarBinding.inflate(LayoutInflater.from(context), viewGroup, false)
 
     init {
-        initView()
-        initData()
-    }
-
-    private fun initView() {
         with(snackBarLayout) {
             removeAllViews()
             setPadding(20, 20, 20, 88)
             setBackgroundColor(ContextCompat.getColor(context, R.color.transparent))
             addView(snackBarBinding.root, 0)
         }
-    }
-
-    private fun initData() {
         snackBarBinding.snackbarTextView.text = message
     }
 
@@ -43,6 +34,6 @@ class SnackBarView(
     }
 
     companion object {
-        fun make(view: View, message: String) = SnackBarView(view, message)
+        fun make(view: ViewGroup, message: String) = SnackBarView(view, message)
     }
 }
